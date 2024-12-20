bb3cdda88fdc7998b61773a88940a89a34616f80
Jegorik <jegorik@aroundacloud.co.uk>
2024-12-20 16:48:05 +0200

    Minor code fixes

--- Day8-Caesar_cipher/caesar_cipher.py
+++ Day8-Caesar_cipher/caesar_cipher.py
@@ -3,19 +3,19 @@ import string
 
 class Cipher:
     logo = """
- ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
-a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
-8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
-"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
- '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88
 
-                          88                                 
- ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
-a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
-8b         88 88       d8 88       88 8PP""""""" 88          
-"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
- '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
-              88                                             
+                          88
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
+8b         88 88       d8 88       88 8PP""""""" 88
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88
+              88
               88
 """
 
@@ -63,7 +63,7 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
             if not string_to_process:
                 print('Input can\'t be empty.')  # Ensure input is not empty
                 continue
-            shift_number = self.get_shift_number(f'Type the shift number. Choose from 0 to {self.alphabet_length}:\n')
+            shift_number = self.get_shift_number(f'Type the shift number. Choose from 0 to {self.alphabet_length-1}:\n')
             return {'string': string_to_process, 'shift_number': shift_number}  # Return the data as a dictionary
 
     def get_shift_number(self, prompt: str) -> int:
